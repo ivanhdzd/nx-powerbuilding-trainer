05915a9671d952a9d15b093d3bb4969d55c1da74
IvanHdzD <ivanhdzd@hotmail.com>
2021-11-25 19:40:03 -0600

    chore(server-auth): Creates server auth app

---
+++ apps/server/auth/src/main.ts
@@ -0,0 +1,22 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { INestApplication, Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app: INestApplication = await NestFactory.create(AppModule);
+  const globalPrefix: string = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port: number = +process.env.PORT || 3333;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
